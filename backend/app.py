adad5101c3c413d5e68d4433433b11ab36d4e91e
Anshika Khare <anshikakhare099@gmail.com>
2025-07-06 23:27:45 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -151,4 +151,7 @@ def test_connection():
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
